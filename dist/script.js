43e28b049b28c8af1a5d4b328d422d178e9cf227
Ailen Ariosto <49838032+AilenAriosto@users.noreply.github.com>
2021-09-11 14:26:38 -0300

    change design, add pokemons, add characteres

--- dist/script.js
+++ dist/script.js
@@ -1,46 +1,86 @@
-const pokemonsIds = [1,2,3,4,5,6,7,8,9,10]
+let swiper = document.getElementById('swiper')
 
-const newArray = pokemonsIds.map(async id =>{
-    const pokemon = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${id}`
-    );
-    const parsedPokemon = await pokemon.json()
-    pintarCard(id, parsedPokemon)
-})
 
+const fetchPokemons = async () => {
+    for (let i = 1; i <= 800; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    pintarCard(id, pokemon)
+}
+
+fetchPokemons()
 
 const pintarCard = (i,pokemon) => {
     let swiper = document.getElementById('swiper')
     let swiperSlide = document.createElement('div')
     swiperSlide.classList.add('swiper-slide')
-    swiper.appendChild(swiperSlide)
-    //Crear elemento titulo y asignarle una variable
+  
+    //TitleName
     let titleName = document.createElement('h1')
-    //Agregarle una clase al titulo
     titleName.classList.add('card-body-title')
-    //Insertar los nombres de los pokemones
-    titleName.innerHTML = `${pokemon.name}`
-    //Crear elemento imagen y asignarle una variable
+    const pokemonName = pokemon.name
+    titleName.innerText = pokemonName
+    
+    //Imagen
     let imageCard = document.createElement('img')
-    //Agregarle una clase a la imagen
     imageCard.classList.add('carousel-item__img')
-    //La imagen es hija de li
-    swiperSlide.appendChild(imageCard)
-    //Insertar las imagenes de los pokemones
     imageCard.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i}.svg`
-    //Ordenar elementos. Titulo está antes que la imagen, y los dos estan dentro de li
-    swiperSlide.insertBefore( titleName,imageCard)
+    
+    //Experiencia
+    let exp = document.createElement('p')
+    exp.classList.add('card-body-parraf')
+    const pokemonExp = pokemon.base_experience
+    exp.innerHTML = `Experiencia: ${pokemonExp}`
+    
+    //Hp
+    let hp = document.createElement('p')
+    hp.classList.add('card-body-parraf')
+    const pokemonHp = pokemon.stats[0].base_stat
+    hp.innerHTML = `Hp: ${pokemonHp}`
+    
+    //Ataque
+    let attack = document.createElement('p')
+    attack.classList.add('card-body-parraf')
+    const pokemonAttack = pokemon.stats[1].base_stat
+    attack.innerHTML = `Attack: ${pokemonAttack}`
+    
+    //Defenda
+    let defense = document.createElement('p')
+    defense.classList.add('card-body-parraf')
+    const pokemonDefense = pokemon.stats[2].base_stat
+    defense.innerHTML = `Defense: ${pokemonDefense}`
+
+    //Special
+    let special = document.createElement('p')
+    special.classList.add('card-body-parraf')
+    const pokemonSpecial = pokemon.stats[3].base_stat
+    special.innerHTML = `Special: ${pokemonSpecial}`
+
+    swiper.appendChild(swiperSlide)
+    swiperSlide.appendChild(titleName)
+    swiperSlide.appendChild(imageCard)
+    swiperSlide.appendChild(exp)
+    swiperSlide.appendChild(hp)
+    swiperSlide.appendChild(attack)
+    swiperSlide.appendChild(defense)
+    swiperSlide.appendChild(special)
+
+    swiperSlide.addEventListener('click', function(){
+        titleName.classList.toggle('hide')
+        imageCard.classList.toggle('hide')
+        
+        exp.classList.toggle('show')
+        hp.classList.toggle('show')
+        attack.classList.toggle('show')
+        defense.classList.toggle('show')
+        special.classList.toggle('show')
+
+    })
 
 }
-
-// const pokemon = {
-//     id: data.id,
-//     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-//     name: data.name,
-//     hp: data.stats[0].base_stat,
-//     exp: data.base_experience,
-//     attack: data.stats[1].base_stat,
-//     defense: data.stats[2].base_stat,
-//     special: data.stats[3].base_stat
-// }
-
